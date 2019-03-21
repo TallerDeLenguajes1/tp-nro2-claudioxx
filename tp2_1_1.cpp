4768733bbcae5687d63c2072dd02c6a1ed26cdb1
BorisAsen <borisyasen@gmail.com>
2019-03-21 18:12:11 -0300

    Tp2 primer commit

---
+++ tp2_1_1.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <conio.h>
+#define N 4
+#define M 5
+
+int main(){
+	int f,c;
+	int mt[N][M];
+
+	for(f = 0;f<N; f++){
+ 		for(c = 0;c<M; c++){
+			mt[f][c]=c;
+		}
+	}
+
+	for(f = 0;f<N; f++){
+ 		for(c = 0;c<M; c++){
+			cprintf("%d", mt[f][c]);
+		}
+		cprintf("\n");
+	}
+	return 0;
+}
